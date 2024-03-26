993ef0d4f9c5d4c02ed4a08c342a7e15b854e08f
DAlinka <drahosalinka@freemail.hu>
2024-03-26 18:30:49 +0100

    first steps

---
+++ Minesweeper/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 618);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "Score:";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked_1);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Score:";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 640);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        private TableLayoutPanel gamePanel; 
+        private Button[,] gameFields;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMenu();
+            InitializeGamePanel();
+        }
+
+        private void InitializeMenu()
+        {
+            // Létrehozunk egy menüsort
+            MenuStrip menuStrip = new MenuStrip();
+
+            // Létrehozunk menüelemeket
+            ToolStripMenuItem newGameMenu = new ToolStripMenuItem("New Game");
+            ToolStripMenuItem exitMenu = new ToolStripMenuItem("Exit");
+
+            // A menüelemekhez hozzáadjuk az eseménykezelőket
+            newGameMenu.Click += NewGameMenuItem_Click;
+            exitMenu.Click += ExitMenuItem_Click;
+
+            // Az "Exit" menüelemhez hozzáadjuk a gyorsbillentyűt
+            exitMenu.ShortcutKeys = Keys.Control | Keys.X;
+
+            // A menüsort hozzáadjuk a formhoz
+            menuStrip.Items.Add(newGameMenu);
+            menuStrip.Items.Add(exitMenu);
+
+            // A formhoz hozzáadjuk a menüsort
+            Controls.Add(menuStrip);
+        }
+
+        private void NewGameMenuItem_Click(object sender, EventArgs e)
+        {
+            // Új játék kezdése
+            // Implementáld ezt a metódust a saját játékod logikájával
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            // Kilépés az alkalmazásból
+            Close();
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void InitializeGamePanel()
+        {
+            gamePanel = new TableLayoutPanel();
+            gamePanel.Dock = DockStyle.Fill;
+            gamePanel.BackColor = Color.LightGray;
+
+            for (int i = 0; i < 10; i++)
+            {
+                gamePanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 30)); // Oszlopok szélessége 30 pixel
+                gamePanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 30)); // Sorok magassága 30 pixel
+            }
+            gamePanel.AutoSize = false;
+
+            // A játékteret hozzáadni a formhoz
+            Controls.Add(gamePanel);
+
+            // Játékmezők létrehozása a Panelen belül
+            CreateGameFields();
+        }
+
+        private void CreateGameFields()
+        {
+            int gridSize = 10; // Például 10x10-es játékteret készítünk
+            gameFields = new Button[gridSize, gridSize]; // Inicializáljuk a gombok tömbjét
+            gamePanel.Padding = new Padding(0, 30, 0, 0);
+
+            // Játékmezők létrehozása és elhelyezése a Panelen belül
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    // Játékmező létrehozása
+                    Button field = new Button();
+                    field.Size = new Size(30, 30);
+                    field.FlatStyle = FlatStyle.Flat; // Kicsit szebb megjelenítés
+                    field.BackColor = Color.White; // Alapértelmezett szín
+                    field.Dock = DockStyle.Fill; // A gomb dockolása Fill-re
+
+                    // Az aktuális cellához adás
+                    gamePanel.Controls.Add(field, j, i);
+
+                    // Gomb eseménykezelőjének hozzáadása
+                    field.Click += Field_Click;
+                }
+            }
+        }
+
+        private void Field_Click(object sender, EventArgs e)
+        {
+            // Az esemény kiváltója (sender) egy gomb, így el tudjuk érni annak tulajdonságait
+            Button clickedField = (Button)sender;
+            // Itt teheted meg, amit a gombra kattintva szeretnél
+            // Például:
+            clickedField.BackColor = Color.Red; // A gomb háttere piros lesz a kattintásra
+        }
+    }
+}
